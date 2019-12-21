7383326690bc16132199e58614400a3e883df006
dzony6969 <dzony6969@users.noreply.github.com>
2019-12-21 18:40:15 +0100

    init

---
+++ js/scroll.js
@@ -0,0 +1,37 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// Select tab content item
+function selectItem(e) {
+  // Remove all show and border classes
+  removeBorder();
+  removeShow();
+  // Add border to current tab item
+
+  console.log(this);
+  this.classList.add("active");
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  console.log(tabContentItem);
+  // Add show class
+  tabContentItem.classList.add("show");
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+  tabItems.forEach(item => {
+    item.classList.remove("active");
+  });
+}
+
+// Remove show class from all content items
+function removeShow() {
+  tabContentItems.forEach(item => {
+    item.classList.remove("show");
+  });
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+  item.addEventListener("click", selectItem);
+});
